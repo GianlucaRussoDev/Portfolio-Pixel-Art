a88eb2261bac934a0c96b9bdd3e0716a4a5b264b
0xHappening <109430749+0xHappening@users.noreply.github.com>
2023-04-24 10:15:42 +0200

    try mail 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
-function SendMAil() {
-    var params {
+function submit() {
+    var params = {
         from_name : document.getElementById("fullName").value,
         email_id : document.getElementById(email_id).value,
         message : document.getElementById("message").value
