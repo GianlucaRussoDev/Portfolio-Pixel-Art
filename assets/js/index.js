109e79d32906c9784d5a4b0da2208ac46ec7f039
GianlucaRussoDev <gianlucarussodev@gmail.com>
2023-11-01 19:55:11 +0100

    v2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,25 @@
-
-
 function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_k2jl6cb";
-    const templateID = "template_yuc1r3o";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-      (function () {
-        emailjs.init("YWiqscEfYONp0qV9Z");
-      })();
-  }
+  (function () {
+    emailjs.init("YWiqscEfYONp0qV9Z");
+  })();
+
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_k2jl6cb";
+  const templateID = "template_yuc1r3o";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!")
+
+    })
+    .catch(err => console.log(err));
+}
